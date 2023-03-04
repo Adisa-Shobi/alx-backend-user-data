9431a3e25ae6f2da6fcd48441c6df614bf14ca4d
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-03-04 13:44:43 +0000

    Create: 'encrypt_password.py, filtered_logger.py' modified

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+'''
+defines user hadling functions
+'''
+import mysql.connector
+import csv
+import re
+from typing import List
+import logging
+import os
+
+
+PII_FIELDS = ('name', 'ssn', 'email', 'phone', 'password')
+print(PII_FIELDS)
+
+
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str):
+    '''
+    returns the log message obfuscated
+    '''
+    log_line = message
+    for field in fields:
+        log_line = re.sub(
+            r'((?<={}=)([^;])*(?=;))'.format(field),
+            r'{}'.format(redaction), log_line)
+    return log_line
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''
+        method to filter values in incoming log records using filter_datum
+        '''
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
+
+
+def get_logger() -> logging.Logger:
+    '''
+    get_logger function that takes no arguments
+    and returns a logging.Logger object
+    '''
+    my_logger = logging.getLogger("user_data")
+    my_logger.propagate = False
+    my_logger.setLevel(logging.INFO)
+    stream_handler = logging.StreamHandler()
+    formatter = RedactingFormatter(list(PII_FIELDS))
+    stream_handler.setFormatter(formatter)
+    my_logger.addHandler(stream_handler)
+    return my_logger
+
+
+def get_db():
+    '''
+    returns a connector to the database
+    (mysql.connector.connection.MySQLConnection object)
+    '''
+    user = os.getenv("PERSONAL_DATA_DB_USERNAME")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD")
+    host = os.getenv("PERSONAL_DATA_DB_HOST")
+    db = os.getenv("PERSONAL_DATA_DB_NAME")
+    return mysql.connector.connect(user=user, password=password,
+                                   host=host, database=db)
+
+
+def main():
+    '''
+    database connection using get_db and retrieve all rows in the
+    users table and display each row
+    '''
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    log = get_logger()
+    for row in cursor:
+        log.info(
+            f"name={row[0]}; email={row[1]}; phone={row[2]}; \
+ssn={row[3]}; password={row[4]}; ip={row[5]}; \
+last_login={row[6]}; user_agent={row[7]}")
+
+
+if __name__ == "__main__":
+    main()
