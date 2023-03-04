10e09f36a74ec900c2167f3a2f39970d2e931f09
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-03-04 14:28:57 +0000

    Update: 'filtered_logger.py' function shortened

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -16,8 +16,7 @@ print(PII_FIELDS)
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    '''returns the log message obfuscated
-    '''
+    '''returns the log message obfuscated'''
     for field in fields:
         message = re.sub(f'((?<={field}=)([^;])*(?={separator}))', redaction,
                           message)
