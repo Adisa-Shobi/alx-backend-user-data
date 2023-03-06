6e3b66b15e9c7afec86ff31d4160875d00ea3287
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-03-06 19:59:26 +0000

    Update: 'api/v1/views/index.py' error page

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def error_401() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - calls error
+    """
+    abort(401)
