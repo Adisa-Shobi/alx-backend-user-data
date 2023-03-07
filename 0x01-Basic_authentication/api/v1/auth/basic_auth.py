93128286c7b7a64cc07d52e0c114235b82ae2668
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-03-07 14:47:20 +0000

    Update: 'api/v1/auth/basic_auth.py' method created

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,9 +3,27 @@
 class BasicAuth that inherits from Auth
 '''
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     '''
     BasicAuth, a variation of Auth
     '''
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        '''
+        returns the Base64 part of the Authorization
+        header for a Basic Authentication
+        '''
+        if authorization_header and isinstance(authorization_header, str):
+            syntax_a = authorization_header.startswith("Basic ")
+            if not syntax_a:
+                return None
+            auth_data = authorization_header.split(" ")[-1]
+            return auth_data
+            # decoded_data = base64.b64decode(auth_data)
+            # return auth_data
+        else:
+            return None
