6b60b51082a20051fc2bfd35d95a82244272d70c
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-03-08 13:36:09 +0000

    Update: 'api/v1/auth/session_auth.py' mthd creates session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,14 +3,21 @@
 Definition of SessionAuth inherits from Auth
 '''
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     '''
     Implemetation of SessionAuth creates authentication sessions
     '''
+    user_id_by_session_id = {}
 
-    # def create_session(self, user_id: str = None) -> str:
-    #    '''
-    #    Initializes by creating a session
-    #    '''
+    def create_session(self, user_id: str = None) -> str:
+        '''
+        Initializes by creating a session
+        '''
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
