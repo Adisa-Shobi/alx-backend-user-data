49a19727e7d57e656cb517cde8212a02f6d44650
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-03-14 17:28:53 +0000

    Update: 'auth.py' destroys session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -73,3 +73,9 @@ class Auth:
         except (InvalidRequestError, NoResultFound):
             return None
         return user
+
+    def destroy_session(self, user_id: int) -> None:
+        '''updates the corresponding user’s session ID to None
+        '''
+        self._db.update_user(user_id, session_id=None)
+        return None
