5ee69eff2e1b60025fc5308baec8251ac312f968
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-03-14 12:16:35 +0000

    Create: 'app.py' minimal flsk app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''
+A basic Flask application with auth
+'''
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def home():
+    '''
+    Home route
+    '''
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
