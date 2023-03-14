540cac1b6e94a15347b75464d72ef1a47762d508
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-03-14 17:07:20 +0000

    Update: 'auth.py' edge cases resolved

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -66,8 +66,8 @@ class Auth:
         '''
         Retrieves user by session_id
         '''
-        # if session_id is None:
-        #    return None
+        if session_id is None:
+            return None
         try:
             user = self._db.find_user_by(session_id=session_id)
         except (InvalidRequestError, NoResultFound):
