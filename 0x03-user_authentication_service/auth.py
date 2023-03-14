16c98186c5f64f6c3d57340c1ef534724a23b7ef
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-03-14 17:15:03 +0000

    Update: 'auth.py' type annotation modified

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -62,7 +62,7 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
         return session_id
 
-    def get_user_from_session_id(session_id: str) -> Union[str, None]:
+    def get_user_from_session_id(session_id: str) -> Union[User, None]:
         '''
         Retrieves user by session_id
         '''
