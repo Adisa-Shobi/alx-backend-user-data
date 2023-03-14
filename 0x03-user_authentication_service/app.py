503a41320ed09e98012f0752dce93a9b1cf393e4
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-03-14 16:49:50 +0000

    Update: 'app.py' new route created

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 A basic Flask application with auth
 '''
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 AUTH = Auth()
@@ -30,6 +30,20 @@ def users():
             {"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    '''Defines login route
+    '''
+    email = request.form.get('email', None)
+    password = request.form.get('password', None)
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+    resp = jsonify({"email": email, "message": "logged in"})
+    resp.set_cookie('session_id', session_id)
+    return resp
 
 
 if __name__ == "__main__":
