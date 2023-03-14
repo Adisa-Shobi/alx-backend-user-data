c90b1f36ecc034d8e52677015937e40f71b4545c
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-03-14 13:56:16 +0000

    Update: 'app.py' new route registers users

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,8 +3,10 @@
 A basic Flask application with auth
 '''
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -14,6 +16,20 @@ def home():
     Home route
     '''
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    '''authenticates users
+    '''
+    email = request.form.get('email', None)
+    password = request.form.get('password', None)
+    try:
+        AUTH.register_user(email, password)
+        return jsonify(
+            {"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
