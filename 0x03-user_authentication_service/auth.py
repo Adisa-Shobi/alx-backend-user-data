aec9e3a9f4e8358fb8afc4014e5a8938df3ac660
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-03-16 10:54:42 +0000

    Update: 'auth.py' new mthd updates password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -94,6 +94,24 @@ class Auth:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
             raise ValueError
+
         reset_token = _generate_uuid()
+
         self._db.update_user(user.id, reset_token=reset_token)
+
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        '''takes reset_token string argument and
+        a password string argument and returns None
+        '''
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+
+        hashed_password = _hash_password(password)
+        self._db.update_user(user.id,
+                             reset_token=None,
+                             hashed_password=hashed_password)
+        return None
--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -11,6 +11,10 @@ auth = Auth()
 
 auth.register_user(email, password)
 
-app.app.run()
-print(auth.get_reset_password_token(email))
-print(auth.get_reset_password_token("unknown@email.com"))
+reset_token = auth.get_reset_password_token(email)
+
+print(auth.valid_login(email, password))
+new_password = "password"
+auth.update_password(reset_token, new_password)
+print(auth.valid_login(email, password))
+print(auth.valid_login(email, new_password))
