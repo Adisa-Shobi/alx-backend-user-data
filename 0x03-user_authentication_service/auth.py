d92284b476f9aff55cd2d9319653935ffaca548d
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-03-16 16:00:19 +0000

    Update: 'auth.py' try block updated

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -58,9 +58,10 @@ class Auth:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
             return None
-        session_id = _generate_uuid()
-        self._db.update_user(user.id, session_id=session_id)
-        return session_id
+        else:
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
 
     def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
         '''
