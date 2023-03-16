09acc1483d72fde05458df0efed67499a93f2826
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-03-16 08:12:57 +0000

    Update: 'auth.py' modified

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -92,7 +92,7 @@ class Auth:
         '''
         try:
             user = self._db.find_user_by(email=email)
-        except (InvalidRequestError, NoResultFound):
+        except NoResultFound:
             raise ValueError
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
